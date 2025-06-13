3cd443a83434f9467f453a2219ecfff861dc7125
Spencer <junovhs@gmail.com>
2025-06-12 22:20:15 -0700

    docs(brain): mark task A1-setup-protocol as complete

---
+++ docs/scripts/target/debug/deps/colorchoice-e6349ad88341260c.d
@@ -0,0 +1,7 @@
+C:\Users\Spencer\brain\docs\scripts\target\debug\deps\libcolorchoice-e6349ad88341260c.rmeta: C:\Users\Spencer\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\colorchoice-1.0.4\src\lib.rs
+
+C:\Users\Spencer\brain\docs\scripts\target\debug\deps\libcolorchoice-e6349ad88341260c.rlib: C:\Users\Spencer\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\colorchoice-1.0.4\src\lib.rs
+
+C:\Users\Spencer\brain\docs\scripts\target\debug\deps\colorchoice-e6349ad88341260c.d: C:\Users\Spencer\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\colorchoice-1.0.4\src\lib.rs
+
+C:\Users\Spencer\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\colorchoice-1.0.4\src\lib.rs:
